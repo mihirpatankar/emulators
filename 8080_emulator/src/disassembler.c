428c1016463338b0970da4290abde117406e6810
Sarvesh Patkar <psarvesh314@gmail.com>
2018-08-07 01:11:40 -0700

    Disassembler almost complete. Code buggy. Gets stuck in loop.

--- 8080_emulator/src/disassembler.c
+++ 8080_emulator/src/disassembler.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 /*
  * disassemble 
@@ -22,6 +23,7 @@ uint16_t disassemble(uint8_t *buffer, uint16_t pc)
     printf("%04x:\t",pc);
 
     switch(buffer[pc]) {
+       
         // Data Transfer
         case 0x40: printf("MOV B, B"); break;
         case 0x41: printf("MOV B, C"); break;
@@ -103,15 +105,15 @@ uint16_t disassemble(uint8_t *buffer, uint16_t pc)
         case 0x36: printf("MVI M, #0x%02x", buffer[pc+1]); bytes++; break;
         case 0x3E: printf("MVI A, #0x%02x", buffer[pc+1]); bytes++; break;
 
-        case 0x01: printf("LXI B, #$%02x%02x", buffer[pc+1], buffer[pc+2]); bytes+=2; break;
-        case 0x11: printf("LXI D, #$%02x%02x", buffer[pc+1], buffer[pc+2]); bytes+=2; break;
-        case 0x21: printf("LXI H, #$%02x%02x", buffer[pc+1], buffer[pc+2]); bytes+=2; break;
-        case 0x31: printf("LXI SP, #$%02x%02x", buffer[pc+1], buffer[pc+2]); bytes+=2; break;
+        case 0x01: printf("LXI B, #0x%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0x11: printf("LXI D, #0x%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0x21: printf("LXI H, #0x%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0x31: printf("LXI SP, #0x%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
     
-        case 0x3A: printf("LDA #%02x%02x", buffer[pc+1], buffer[pc+2]); bytes+=2; break;
-        case 0x32: printf("STA #%02x%02x", buffer[pc+1], buffer[pc+2]); bytes+=2; break;
-        case 0x2A: printf("LHLD #%02x%02x", buffer[pc+1], buffer[pc+2]); bytes+=2; break;
-        case 0x22: printf("SHLD #%02x%02x", buffer[pc+1], buffer[pc+2]); bytes+=2; break;
+        case 0x3A: printf("LDA $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0x32: printf("STA $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0x2A: printf("LHLD $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0x22: printf("SHLD $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
 
         case 0x0A: printf("LDAX B"); break;
         case 0x1A: printf("LDAX D"); break;
@@ -121,12 +123,203 @@ uint16_t disassemble(uint8_t *buffer, uint16_t pc)
         case 0xEB: printf("XCHG"); break;
 
         // Arithmetic
+        case 0x80: printf("ADD B"); break;
+        case 0x81: printf("ADD C"); break;
+        case 0x82: printf("ADD D"); break;
+        case 0x83: printf("ADD E"); break;
+        case 0x84: printf("ADD H"); break;
+        case 0x85: printf("ADD L"); break;
+        case 0x86: printf("ADD M"); break;
+        case 0x87: printf("ADD A"); break;
+        
+        case 0xC6: printf("ADI #0x%02x", buffer[pc+1]); bytes++; break;
+
+        case 0x88: printf("ADC B"); break;
+        case 0x89: printf("ADC C"); break;
+        case 0x8A: printf("ADC D"); break;
+        case 0x8B: printf("ADC E"); break;
+        case 0x8C: printf("ADC H"); break;
+        case 0x8D: printf("ADC L"); break;
+        case 0x8E: printf("ADC M"); break;
+        case 0x8F: printf("ADC A"); break;
+        
+        case 0xCE: printf("ACI #0x%02x", buffer[pc+1]); bytes++; break;
+                   
+        case 0x90: printf("SUB B"); break;
+        case 0x91: printf("SUB C"); break;
+        case 0x92: printf("SUB D"); break;
+        case 0x93: printf("SUB E"); break;
+        case 0x94: printf("SUB H"); break;
+        case 0x95: printf("SUB L"); break;
+        case 0x96: printf("SUB M"); break;
+        case 0x97: printf("SUB A"); break;
+        
+        case 0xD6: printf("SUI #0x%02x", buffer[pc+1]); bytes++; break;
+
+        case 0x98: printf("SBB B"); break;
+        case 0x99: printf("SBB C"); break;
+        case 0x9A: printf("SBB D"); break;
+        case 0x9B: printf("SBB E"); break;
+        case 0x9C: printf("SBB H"); break;
+        case 0x9D: printf("SBB L"); break;
+        case 0x9E: printf("SBB M"); break;
+        case 0x9F: printf("SBB A"); break;
+        
+        case 0xDE: printf("SBI #0x%02x", buffer[pc+1]); bytes++; break;
+
+        case 0x04: printf("INR B"); break;
+        case 0x0C: printf("INR C"); break;
+        case 0x14: printf("INR D"); break;
+        case 0x1C: printf("INR E"); break;
+        case 0x24: printf("INR H"); break;
+        case 0x2C: printf("INR L"); break;
+        case 0x34: printf("INR M"); break;
+        case 0x3C: printf("INR A"); break;
+
+        case 0x05: printf("DCR B"); break;
+        case 0x0D: printf("DCR C"); break;
+        case 0x15: printf("DCR D"); break;
+        case 0x1D: printf("DCR E"); break;
+        case 0x25: printf("DCR H"); break;
+        case 0x2D: printf("DCR L"); break;
+        case 0x35: printf("DCR M"); break;
+        case 0x3D: printf("DCR A"); break;
+
+        case 0x03: printf("INX B"); break;
+        case 0x13: printf("INX D"); break;
+        case 0x23: printf("INX H"); break;
+        case 0x33: printf("INX SP"); break;
+                   
+        case 0x0B: printf("DCX B"); break;
+        case 0x1B: printf("DCX D"); break;
+        case 0x2B: printf("DCX H"); break;
+        case 0x3B: printf("DCX SP"); break;
+                   
+        case 0x09: printf("DAD B"); break;
+        case 0x19: printf("DAD D"); break;
+        case 0x29: printf("DAD H"); break;
+        case 0x39: printf("DAD SP"); break;
+                   
+        case 0x27: printf("DAA"); break;
+
         // Logical
+        case 0xA0: printf("ANA B"); break;
+        case 0xA1: printf("ANA C"); break;
+        case 0xA2: printf("ANA D"); break;
+        case 0xA3: printf("ANA E"); break;
+        case 0xA4: printf("ANA H"); break;
+        case 0xA5: printf("ANA L"); break;
+        case 0xA6: printf("ANA M"); break;
+        case 0xA7: printf("ANA A"); break;
+        
+        case 0xE6: printf("ANI #0x%02x", buffer[pc+1]); bytes++; break;
+
+        case 0xA8: printf("XRA B"); break;
+        case 0xA9: printf("XRA C"); break;
+        case 0xAA: printf("XRA D"); break;
+        case 0xAB: printf("XRA E"); break;
+        case 0xAC: printf("XRA H"); break;
+        case 0xAD: printf("XRA L"); break;
+        case 0xAE: printf("XRA M"); break;
+        case 0xAF: printf("XRA A"); break;
+        
+        case 0xEE: printf("XRI #0x%02x", buffer[pc+1]); bytes++; break;
+                   
+        case 0xB0: printf("ORA B"); break;
+        case 0xB1: printf("ORA C"); break;
+        case 0xB2: printf("ORA D"); break;
+        case 0xB3: printf("ORA E"); break;
+        case 0xB4: printf("ORA H"); break;
+        case 0xB5: printf("ORA L"); break;
+        case 0xB6: printf("ORA M"); break;
+        case 0xB7: printf("ORA A"); break;
+        
+        case 0xF6: printf("ORI #0x%02x", buffer[pc+1]); bytes++; break;
+        
+        case 0xB8: printf("CMP B"); break;
+        case 0xB9: printf("CMP C"); break;
+        case 0xBA: printf("CMP D"); break;
+        case 0xBB: printf("CMP E"); break;
+        case 0xBC: printf("CMP H"); break;
+        case 0xBD: printf("CMP L"); break;
+        case 0xBE: printf("CMP M"); break;
+        case 0xBF: printf("CMP A"); break;
+        
+        case 0xFE: printf("CPI #0x%02x", buffer[pc+1]); bytes++; break;
+
+        case 0x07: printf("RLC"); break;
+        case 0x0F: printf("RRC"); break;
+        case 0x17: printf("RAL"); break;
+        case 0x1F: printf("RAR"); break;
+        case 0x2F: printf("CMA"); break;
+        case 0x3F: printf("CMC"); break;
+        case 0x37: printf("STC"); break;
+
         // Branch
+        case 0xC3: printf("JMP $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+
+        case 0xC2: printf("JNZ $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xCA: printf("JZ $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xD2: printf("JNC $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xDA: printf("JC $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xE2: printf("JPO $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xEA: printf("JPE $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xF2: printf("JP $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xFA: printf("JM $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+
+        case 0xCD: printf("CALL $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+ 
+        case 0xC4: printf("CNZ $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xCC: printf("CZ $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xD4: printf("CC $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xDC: printf("CC $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xE4: printf("CPO $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xEC: printf("CPE $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xF4: printf("CP $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        case 0xFC: printf("CM $%02x%02x", buffer[pc+2], buffer[pc+1]); bytes+=2; break;
+        
+        case 0xC9: printf("RET"); break;
+ 
+        case 0xC0: printf("RNZ"); break;
+        case 0xC8: printf("RZ"); break;
+        case 0xD0: printf("RC"); break;
+        case 0xD8: printf("RC"); break;
+        case 0xE0: printf("RPO"); break;
+        case 0xE8: printf("RPE"); break;
+        case 0xF0: printf("RP"); break;
+        case 0xF8: printf("RM"); break;
+        
+        case 0xC7: printf("RST 0"); break;
+        case 0xCF: printf("RST 1"); break;
+        case 0xD7: printf("RST 2"); break;
+        case 0xDF: printf("RST 3"); break;
+        case 0xE7: printf("RST 4"); break;
+        case 0xEF: printf("RST 5"); break;
+        case 0xF7: printf("RST 6"); break;
+        case 0xFF: printf("RST 7"); break;
+        
         // Stack, I/O and Machine Control
+        case 0xC5: printf("PUSH B"); break;
+        case 0xD5: printf("PUSH D"); break;
+        case 0xE5: printf("PUSH H"); break;
+        case 0xF5: printf("PUSH PSW"); break;
+
+        case 0xC1: printf("POP B"); break;
+        case 0xD1: printf("POP D"); break;
+        case 0xE1: printf("POP H"); break;
+        case 0xF1: printf("POP PSW"); break;
+                 
+        case 0xDB: printf("IN  #0x%02x", buffer[pc+1]); bytes++; break;
+        case 0xD3: printf("OUT #0x%02x", buffer[pc+1]); bytes++; break;
+
+        case 0xE3: printf("XTHL"); break;
+        case 0xF9: printf("SPHL"); break;
+        case 0xFB: printf("EI"); break;
+        case 0xF3: printf("DI"); break;
+        case 0x76: printf("HLT"); break;
         case 0x00: printf("NOP"); break;    
-    
-    
+        default:
+            printf("Error: Unexpected opcode"); bytes = -1; break;    
     }
 
     printf("\n");
@@ -134,9 +327,34 @@ uint16_t disassemble(uint8_t *buffer, uint16_t pc)
     return bytes;
 }
 
+/*
+ * Disassembler Main
+ */
 
 int main(int argc, char* argv[])
 {
+    if(argc != 2) {
+        printf("Usage: disassembler <file>\n");
+        return 1;
+    }
 
+    FILE *fp = fopen(argv[1], "rb");
+    if(fp == NULL) {
+        printf("fopen error\n");
+        return 2;
+    }
 
+    fseek(fp, 0L, SEEK_END);
+    int fsize = ftell(fp);
+    fseek(fp, 0L, SEEK_SET);
+
+    uint8_t *buffer = (uint8_t *) malloc(fsize);
+    
+    fread(buffer, fsize, 1, fp);
+    fclose(fp);
+    
+    uint16_t pc = 0;
+    while(pc < fsize)
+        pc += disassemble(buffer, pc);
+    return 0;
 }
